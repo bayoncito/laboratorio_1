69973d6f03cf6ba68bf6a2e1eb805c99b3e27c3f
Andres Bayon <andresbayon22@gmail.com>
2024-02-29 20:17:56 -0500

    Start Project

---
+++ laboratorio_xd/lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Aplicacion Test',
+        theme: ThemeData(
+          useMaterial3: true, //Estilo de mteriales para toda la aplicacion
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: Color.fromARGB(201, 12, 233, 245)),
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var pair = appState.current;
+
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('A random WESOME idea:'),
+          BigCard(pair: pair),
+          ElevatedButton(
+            onPressed: () {
+              //print('botón presionado!');
+              appState.getNext();
+            },
+            child: Text('Siguiente'),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(pair.asLowerCase);
+  }
+}
